8f3df28f7e9872da6c75fe39d695a42fda2b3963
ROHIT KUMAR <74416087+dynamiccoder-gif@users.noreply.github.com>
2026-03-03 14:34:44 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,8 @@
 lists=['apple', 'banana', 'cherry', 'rohit kumar', 'rohit baghel','aditiya pandit', 'siddthan rana', 'ashwini kumar']
 max_number = lambda n : len(n)
 maximum =0
-max_number= list(map(max_number, lists))
-print(max_number)
-def max(n):
-    global maximum
-   
-    if(n>maximum):
-        maximum=n
-        
-        return True
-        
-    else:
-        return True
-        
-list(filter(max, max_number))
-print("maximum is of length:", maximum)
+max_number_list= list(map(max_number, lists))
+max_number =max(max_number_list)
+
+max_word = list(filter(lambda x: len(x) == max_number, lists))
+print(max_word)
