4cd6a8fe90d752d4951299adf1f378ebaf8a89fc
ROHIT KUMAR <74416087+dynamiccoder-gif@users.noreply.github.com>
2026-03-03 14:43:51 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 lists=['apple', 'banana', 'cherry', 'rohit kumar', 'rohit baghel','aditiya pandit', 'siddthan rana', 'ashwini kumar']
-max_number = lambda n : len(n)
-maximum =0
-max_number_list= list(map(max_number, lists))
+word_sizer = lambda n : len(n)
+max_number_list= list(map(word_sizer, lists))
 max_number =max(max_number_list)
 
 max_word = list(filter(lambda x: len(x) == max_number, lists))
